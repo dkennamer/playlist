23b4b2a1fb60149359f34f43a1f4f3355fe5aee2
Dylan Kennamer <dylan.kennamer@gmail.com>
2013-07-20 22:35:04 +0900

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Playlist::Application.config.secret_key_base = '67f65095cdad04c8fbf7609f73f209f088f01319947ccea7bbee1df130b5a9d20ae6d1713e17f95b5a9e42fc7417eab3b4f3e9c53a171a6b83b0eeb0c81eb6a0'
---
+++ vendor/bundle/gems/json-1.8.0/ext/json/ext/parser/siteconf20130720-18355-gr0bqp.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20130720-18355-yy4u6q"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
